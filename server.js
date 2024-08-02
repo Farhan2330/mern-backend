ab675036cc6ec9849ec8c92e0dfd85354d1fe693
Farhan2330 <farhanahmed31943@gmail.com>
2024-08-03 04:48:27 +0530

    backend initialised

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+
+import userRouter from "./routes/userRoute.js"
+import taskRouter from "./routes/taskRoute.js"
+import forgotPasswordRouter from "./routes/forgotPassword.js"
+
+//app config
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8001
+mongoose.set('strictQuery', true);
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//db config
+const mongoUrl = process.env.MONGO_URI;
+console.log(mongoUrl);
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+}, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("DB Connected")
+    }
+})
+
+//api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/task", taskRouter)
+app.use("/api/forgotPassword", forgotPasswordRouter)
+
+//listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`))
